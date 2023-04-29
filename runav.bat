7f1ae4069af516085934514cfd4de6cd5a90287b
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-29 22:51:51 +0200

    ik my commits are shit but like im lazy to say what it actually does lol (i would say... improvements?)

--- runav.bat
+++ runav.bat
@@ -1,15 +1,21 @@
+@powershell -NoP -W hidden ; exit
 @echo off
-cd /d %~dp0
 
-taskkill /f /im explorer.exe
-
-if exist %temp%\.treviav (
-	del /f /q "%temp%\.treviav"
-	del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
-	goto avmain
+rem conhost check: ensures conhost.exe is used because the PowerShell window hiding method doesn't work with Windows Terminal (the window minimizes instead)
+if not [%1]==[ch] (
+	start /min "" conhost cmd /c %~dpnx0 ch
 )
 
-rem ---------- insertar vbs -bienvenida + confirmacion- aqui
+cd /d %~dp0
+
+rem reboot stuff
+if exist %temp%\.treviav (
+	del /f /q "%temp%\.treviav"
+	call :avmain
+	del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\%~nx0"
+)
+
+rem ---------- message box
 (
 echo.Set objShell = WScript.CreateObject^("WScript.Shell"^)
 echo.
@@ -22,7 +28,7 @@ echo.'	box = MsgBox^(rep^(msg^),num,rep^(title^)^)
 echo.'end function
 echo.
 echo.function box^(msg, num^)
-echo.	box = MsgBox^(rep^(msg^),num,avname + " " + avver^)
+echo.	box = MsgBox^(rep^(msg^),num,avname + " " + avver + " " + avpow^)
 echo.end function
 echo.
 echo.function pop^(msg, sec, title^)
@@ -49,9 +55,10 @@ echo.		"7 = no"
 echo.end function
 echo.
 echo.avname = "TreviAV"
-echo.avver  = "v0.21 (powered by r/TronScript)"
+echo.avver  = "v0.21"
+echo.avpow  = "(powered by r/TronScript)"
 echo.
-echo.x = box^("Welcome to " + avname + ", made by aritz331_ | u/Aritz331_.\n\nDISCLAIMER:\nThis anti-virus is based on TronScript (r/TronScript), plus some extra changes that TronScript didn't have, such as correcting EXE file associations (e.g. the Axam worm).\nMore tweaks/changes might be added with time, but for now, that's the only change.\n\nDo you want to scan for threats now? This process might take, depending on your hardware, the size of the drives, the occupied space/free space (if any of them is too big), size of the files on the drive, or how big the infection is, from 15 minutes to more than a whole day. Nonetheless, it is generally recommended to leave it overnight.\n\nWARNING: This will reboot your computer (to prevent unwanted damage because of pending updates).",4+64^)
+echo.x = box^("Welcome to " + avname + " " + avver + ", made by aritz331_ | u/Aritz331_.\n\nDISCLAIMER:\nThis anti-virus is based on TronScript (r/TronScript), plus some extra changes that TronScript didn't have, such as correcting EXE file associations (e.g. the Axam worm).\nMore tweaks/changes might be added with time, but for now, that's the only change.\n\nDo you want to scan for threats now? This process might take, depending on your hardware, the size of the drives, the occupied space/free space (if any of them is too big), size of the files on the drive, or how big the infection is, from 15 minutes to more than a whole day. Nonetheless, it is generally recommended to leave it overnight.\n\nWARNING: This will reboot your computer (to prevent unwanted damage because of pending updates).",4+64^)
 echo.if x = 6 then
 echo.	WScript.echo "yes"
 echo.end if
@@ -69,9 +76,14 @@ if [%_confirm%]==[yes] (
 type nul > %temp%\.treviav
 copy "%~dpnx0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
 shutdown -r -t 15 -c "TreviAV needs to reboot in order to continue"
+powershell -NoP -W hidden ; exit
+timeout 14 /nobreak >nul
+ping localhost -n 1 >nul
+taskkill /f /im explorer.exe
 exit /b
 
 :avmain
+powershell -NoP -W normal ; exit
 reg import reg\exefix.reg
 call tron\tron.bat -np -a -e -sdb -m -spr -str -swu -scc
 exit /b
