d110f75919814a76e1e6c0053132854a719c4007
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-04-30 23:23:55 +0200

    delete tron

--- tron/resources/functions/log.bat
+++
@@ -1,7 +0,0 @@
-:: Tron's log function
-
-:: When the whole argument (%1) string is wrapped in double quotes, it is sent as an argument.
-:: The tilde syntax (%~1) removes the double quotes around the argument.
-@echo off
-echo:%~1>> "%LOGPATH%\%LOGFILE%"
-echo:%~1
--- tron/resources/functions/log_with_date.bat
+++
@@ -1,7 +0,0 @@
-:: Tron's log function with embedded date and time group (DTG)
-
-:: When the whole argument (%1) string is wrapped in double quotes, it is sent as an argument.
-:: The tilde syntax (%~1) removes the double quotes around the argument.
-@echo off
-echo:%CUR_DATE% %TIME% %~1>> "%LOGPATH%\%LOGFILE%"
-echo:%CUR_DATE% %TIME% %~1
