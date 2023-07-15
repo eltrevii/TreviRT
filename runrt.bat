30b5774b97b3930c9483d5238588fb0c100bb6a0
Aritz T <aritz331@gmail.com>
2023-07-15 21:42:28 +0000

    ...maybemaybemaybe

--- runrt.bat
+++ runrt.bat
@@ -69,17 +69,18 @@ echo.end function
 echo.
 echo.avname = "%_avname%"
 echo.avver  = "%_avver%"
+echo.avmsg  = "%_avmsg%"
 echo.avpow  = "(powered by r/TronScript)"
 echo.
-echo.x = box^("%_avmsg%DISCLAIMER:\nThis extension is based on TronScript (r/TronScript), plus some extra changes that TronScript didn't have, such as correcting EXE file associations (e.g. the Axam worm).\nMore tweaks/changes might be added with time, but for now, that's the only change.\n\nThere's an extra feature which is to reboot before proceeding. TronScript does have a parameter to reboot, but it doesn't start after rebooting (because it only allows to reboot to Safe Mode).\n\nDo you want to scan for threats now? This process might take, depending on your hardware, the size of the drives, the occupied space/free space (if any of them is too big), size of the files on the drive, or how big the infection is, from 15 minutes to more than a whole day. Nonetheless, it is generally recommended to leave it overnight.\n\nWARNING: This will reboot your computer (to prevent unwanted damage because of pending updates).",4+64^)
+echo.x = box^(avmsg ^& WScript.Arguments(0),4+64^)
 echo.if x = 6 then
 echo.	WScript.echo "yes"
 echo.end if
-)> %temp%\treviav.vbs
+)> %temp%\trtyesno.vbs
 rem ----------
 
-for /f %%i in ('cscript //NOLOGO %temp%\treviav.vbs') do (set "_confirm=%%i")
-del /f /q %temp%\treviav.vbs
+for /f %%i in ('cscript //NOLOGO %temp%\trtyesno.vbs "DISCLAIMER:\nThis extension is based on TronScript (r/TronScript), plus some extra changes that TronScript didn't have, such as correcting EXE file associations (e.g. the Axam worm).\nMore tweaks/changes might be added with time, but for now, that's the only change.\n\nThere's an extra feature which is to reboot before proceeding. TronScript does have a parameter to reboot, but it doesn't start after rebooting (because it only allows to reboot to Safe Mode).\n\nDo you want to scan for threats now? This process might take, depending on your hardware, the size of the drives, the occupied space/free space (if any of them is too big), size of the files on the drive, or how big the infection is, from 15 minutes to more than a whole day. Nonetheless, it is generally recommended to leave it overnight.\n\nWARNING: This will reboot your computer (to prevent unwanted damage because of pending updates)."') do (set "_confirm=%%i")
+
 if [%_confirm%]==[yes] (
 	goto avreb
 )
@@ -94,7 +95,7 @@ rem copy "%~f0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\" >nul
 	echo powershell start -verb runas cmd /c "%~f0" -startup
 	echo del /f /q %~f0
 	echo exit
-) > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\treviav.bat"
+) > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\trevirt.bat"
 
 shutdown -r -t 5 -c "TreviAV needs to reboot in order to continue. Rebooting in 5 seconds."
 timeout 4 /nobreak >nul
@@ -120,44 +121,15 @@ exit /b
 :update
 set "_url=https://raw.githubusercontent.com/aritz331/TreviAV/main"
 
-curl -#L "%_url%/runav.bat" -o "%temp%\.treviavupd"
-fc "%~f0" "%temp%\.treviavupd" || call :upddiag
+curl -#L "%_url%/runrt.bat" -o "%temp%\.trtupd"
+fc "%~f0" "%temp%\.trtupd" || call :upddiag
 exit /b
 
 :upddiag
-(
-echo.Set objShell = WScript.CreateObject^("WScript.Shell"^)
-echo.
-echo.function rep^(msg^)
-echo.	rep = Replace^(msg, "\n", vbCrLf^)
-echo.end function
-echo.
-echo.'function box^(msg, num, title^)
-echo.'	box = MsgBox^(rep^(msg^),num,rep^(title^)^)
-echo.'end function
-echo.
-echo.function box^(msg, num^)
-echo.	box = MsgBox^(rep^(msg^),num,avname + " " + avver + " " + avpow^)
-echo.end function
-echo.
-echo.function pop^(msg, sec, title^)
-echo.	objShell.Popup msg,sec,title
-echo.end function
-echo.
-echo.avname = "%_avname%"
-echo.avver  = "%_avver%"
-echo.avpow  = "(powered by r/TronScript)"
-echo.
-echo.x = box^("%_avmsg%A new update is available. Do you want to update now?",4+64^)
-echo.if x = 6 then
-echo.	WScript.echo "yes"
-echo.end if
-)> %temp%\treviav.vbs
-
-for /f %%i in ('cscript //NOLOGO %temp%\treviav.vbs') do (set "_updc=%%i")
+for /f %%i in ('cscript //NOLOGO %temp%\trtyesno.vbs') do (set "_updc=%%i")
 
 if [%_updc%]==[yes] (
-	start conhost cmd /c timeout 1 ^& move "%temp%\.treviavupd" "%~f0" ^& call "%~f0" ch
+	start conhost cmd /c timeout 1 ^& move "%temp%\.trtupd" "%~f0" ^& call "%~f0" ch
 	exit
 )
 exit /b
