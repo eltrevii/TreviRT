b91f817c999aa034b7853dbf65c87d382492699e
Aritz T <aritz331@gmail.com>
2023-07-16 17:33:17 +0000

    it might work but

--- runrt.bat
+++ runrt.bat
@@ -2,7 +2,7 @@
 @echo off
 
 rem conhost check: ensures conhost.exe is used because the PowerShell window hiding method doesn't work with Windows Terminal (the window minimizes instead)
-if not [%~1]==[ch] (
+if not [%*]==[ch] (
  	start "" conhost cmd /c "%~f0" %* ch
  	exit /b
 )
